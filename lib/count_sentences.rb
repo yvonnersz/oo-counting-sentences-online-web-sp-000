ae3802f2c89b4bef45eb1b9fe165a79f6ee501b0
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-03 09:11:29 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true:false
   end
 
   def question?
-
+    self.end_with?("?") ? true:false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true:false
   end
 
   def count_sentences
-
+    array = self.split(/[.?!]/)
+    array.reject {|sentence| sentence == ""}.count
   end
 end
